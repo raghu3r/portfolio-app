dcf3a76d5019673ab04dc7d4a505ae68fb4a5635
Raghu <konidelaraghu@gmail.com>
2021-06-25 18:31:54 +0530

    Pre-redering, Data fetcting & Dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
-export default function Home() {
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -10,6 +23,22 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>I am Raghu Rami Reddy. Specialised in Web Development and a Photographer if I get a Lens on! I love playing football and Volleyball.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>          
+          ))}
+        </ul>
       </section>
     </Layout>
   )
